19ba7853069f16494d28f47cf3733cfec1af91d4
shashank77665 <109029541+shashank77665@users.noreply.github.com>
2023-10-26 22:44:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll",function() {
+    header.classList.toggle ("sticky",window.scrollY>120);
+});
+
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.navlist');
+
+menu.onclick=()=>{
+    menu.classList.toggle('bx-menu');
+    navlist.classList.toggle('active');
+};
+
+window.onscroll=()=>{
+    menu.classList.remove('bx-menu');
+    navlist.classList.remove('remove');
+};
